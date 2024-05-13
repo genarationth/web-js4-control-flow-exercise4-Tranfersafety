0c55274664c18b967df3fc0dbbe0f6ac194b3901
Tranfersafety <ratthaphumi.safe@gmail.com>
2024-05-13 17:22:55 +0700

    first commit

---
+++ ternary.js
@@ -0,0 +1,38 @@
+// const dayNumber = 'aaa'
+// if (dayNumber === 0 ){
+//     console.log("sunday");
+// }else if (dayNumber === 1){
+//     console.log("monday");
+// }else if (dayNumber === 2){
+//     console.log("tuesday");
+// }else if (dayNumber === 3){
+//     console.log("wednesday");
+// }else if (dayNumber === 4){
+//     console.log("thursday");
+// }else if (dayNumber === 5){
+//     console.log("friday");
+// }else if (dayNumber === 6){
+//     console.log("saturday");
+// }else{
+//     console.log("Invalid day number");
+// }
+
+const dayNumber = 0;
+
+  dayNumber === 0
+    ? console.log("sunday")
+    : dayNumber === 1
+    ? console.log("monday")
+    : dayNumber === 2
+    ? console.log("tuesday")
+    : dayNumber === 3
+    ? console.log("wednesday")
+    : dayNumber === 4
+    ? console.log("thursday")
+    : dayNumber === 5
+    ? console.log("friday")
+    : dayNumber === 6
+    ? console.log("saturday")
+    : console.log("Invalid day number");
+
+
